a6e2f789316db9cefa129f184448271c88ef5c2e
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-06-27 01:36:11 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -4,18 +4,19 @@
 #include <string.h>
 /**
  * rev_string- function to reverse string
- * @n:parameters
+ * @s:parameters
  */
 void rev_string(char *s)
 {
 	int len = strlen(s);
 	int j;
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
-	for ( j = len - 1 ;j >= 0; j--)
+	for (j = len - 1 ; j >= 0 ; j--)
 	{
 		putchar(s[j]);
 	}

cd8b68aa0f4e5c46bfbef470f694cd39c888ba2a
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-06-26 19:38:09 -0700

    swaps the values of two integers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,13 @@
 #include "main.h"
-
+/**
+ * swap_int - function to swap two numbers
+ * @a: parameter
+ * @b:parameter
+ */
 void swap_int(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
 	*a = *b;
 	*b = temp;

297320c870f635794af8898d40b578c1c4759ee6
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-06-27 02:12:44 -0700

    reverses a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,12 +11,12 @@ void rev_string(char *s)
 	int len = strlen(s);
 	int j = 0;
 	int x = len - 1;
-	for (j =0 ;j < x ; j++, x--)
+
+	for (j = 0 ; j < x ; j++, x--)
 	{
 		char tmp = s[j];
+
 		s[j] = s[x];
 		s[x] = tmp;
-		
 	}
-	putchar('\n');
 }

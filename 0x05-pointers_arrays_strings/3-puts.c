e82f152a5651fd3f5eafd31325debddb19b75e2a
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-06-27 00:48:22 -0700

    reversing

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * _puts- write a string
+ * @str: parameter
+ */
+void _puts(char *str)
+{
+
+	puts(str);
+}
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_rev- print a string in reverse
+ * @s: parameter
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	int len;
+	
+	if (s == NULL)
+	{
+		printf("invalid string\n");
+		return;
+	}
+
+	len = strlen(s); /*check length of string*/
+
+	while (s[len] != '\0') /* check length of string by iterating until '\0' */
+	{
+		len++;
+	}
+	for (i = len-1;i>=0;i--) /* iterates characters in reverse order*/
+	{
+		_putchar(s[i]);
+	}
+	putchar('\n');
+}
+

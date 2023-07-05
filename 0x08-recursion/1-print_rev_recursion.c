caa564b89b4efa3c1ba0c7888f7248127328c83f
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-07-05 05:49:58 -0700

    string reverse recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s:parameter
+ */
+void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+	}
+	_putchar(*s);	
+}

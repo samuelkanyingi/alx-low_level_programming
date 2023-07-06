2800b7add9104f1e64f65e745591303c17d34fdb
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-07-05 21:04:02 -0700

    length of a string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -4,7 +4,6 @@
  * _print_rev_recursion - prints a string in reverse.
  * @s:parameter
  */
-void _print_rev_recursion(char *s);
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s:parameter
+ * Return: string length
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}

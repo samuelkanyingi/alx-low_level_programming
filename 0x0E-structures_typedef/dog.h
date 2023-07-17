70a57c588c53671d4ed0970df722761e79a86d07
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-07-17 14:42:22 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef POPPY_H
+#define POPPY_H
+/**
+ * struct dog - data for dog
+ * @name: dog name
+ * @age:age of dog
+ * @owner:keeper of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif

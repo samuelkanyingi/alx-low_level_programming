7cce48d0c9949b5a213820410baa6adb273484a3
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-07-29 05:24:21 -0700

    singly linked list

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+void printStuff(void) __attribute__((constructor));
+/**
+ * printStuff- to be exsecuted before main
+ */
 void printStuff(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
--- 0x12-singly_linked_lists/100-main.c
+++ 0x12-singly_linked_lists/100-main.c
@@ -7,7 +7,6 @@
  */
 int main(void)
 {
-printStuff();
 printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
 return (0);
 }

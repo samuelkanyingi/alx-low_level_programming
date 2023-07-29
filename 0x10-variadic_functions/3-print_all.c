bde715038e05b312aa1bc3f4ba6f56ece56da001
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-07-29 15:18:30 -0700

    variadic

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -3,49 +3,86 @@
 #include <stdio.h>
 
 /**
+ * p_char-prints character
+ * @arg:argument tpe of char
+ */
+void p_char(va_list arg)
+{
+	char c;
+
+	c = va_arg(arg, int);
+	printf("%c", c);
+}
+/**
+ * p_int-prints an int
+ * @arg:argument of type int
+ */
+void p_int(va_list arg)
+{
+	int i;
+
+	i = va_arg(arg, int);
+	printf("%d", i);
+}
+/**
+ * p_float-prints a float
+ * @arg:argument type float
+ */
+void p_float(va_list arg)
+{
+	double f;
+
+f = va_arg(arg, double);
+	printf("%f", f);
+}
+/**
+ * p_string-prints a string
+ * @arg:argument typ is string
+ */
+void p_string(va_list arg)
+{
+	char *s;
+
+	s = va_arg(arg, char*);
+	if (s == NULL)
+	{
+		printf("(nil)");
+	}
+	else
+	{
+		printf("%s", s);
+	}
+}
+/**
  * print_all- function that prints anything.
  * @format:type
  * ...:variable types
  */
 void print_all(const char * const format, ...)
 {
-char c;
-int i;
-float f;
-char *s;
 int index_n = 0;
-
 va_list args;
 va_start(args, format);
+
 while (format[index_n] != '\0')
 {
 if (format[index_n] == 'c')
 {
-c = va_arg(args, int);
-printf("%c", c);
+p_char(args);
 }
 else if (format[index_n] == 'i')
 {
-i = va_arg(args, int);
-printf("%d", i);
+p_int(args);
 }
 else if (format[index_n] == 'f')
 {
-f = va_arg(args, double);
-printf("%f", f);
+p_float(args);
 }
 else if (format[index_n] == 's')
 {
-s = va_arg(args, char*);
-if (s == NULL)
-{
-printf("(nil)");
+p_string(args);
 }
-else
-{
-printf("%s", s);
-}
-}
+
 if (format[index_n + 1] != '\0' &&
 (format[index_n + 1] != 'i' || format[index_n + 1] == 'c'
 || format[index_n + 1] == 'f' || format[index_n + 1] == 's'))
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,12 @@
 #ifndef SUM_H
 #define SUM_H
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void p_int(va_list arg);
+void p_char(va_list arg);
+void p_float(va_list arg);
+void p_string(va_list arg);
 #endif

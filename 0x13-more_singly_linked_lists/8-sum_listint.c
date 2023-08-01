f3c407657850b6bf87bec113037d633eb6adaa3c
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-08-01 04:28:19 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,21 +4,22 @@
 /**
  * sum_listint-function that returns the sum of nodes in linked list
  * @head:pointer to head
- * Return sum of all data in nodes or 0 is list is empty
+ * Return: sum of all data in nodes or 0 is list is empty
  */
 int sum_listint(listint_t *head)
 {
 	int s = 0;
+	listint_t *m = head;
 
 	if (head == NULL)
 	{
 		return (0);
 	}
 
-	while (head != NULL)
+	while (m != NULL)
 	{
-		s = s + head->n;
-		head = head->next;
+		s = s + m->n;
+		m = m->next;
 	}
 	return (s);
 }

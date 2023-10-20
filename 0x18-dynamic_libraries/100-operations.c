ee53e4631d05731a5a02133f0e810f9224a66c8f
samuelkanyingi <samuelkanyingi2016@gmail.com>
2023-10-20 00:18:30 -0700

    c again

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,68 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * add- function to add
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - function to subtract
+ * @a: first integer
+ * @b: second integer
+ * Return: difference
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function to multiply
+ * @a: first integer
+ * @b: second integer
+ * Return: product
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function to divide
+ * @a: first integer
+ * @b: second integer
+ * Return: division
+ */
+int _div(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/**
+ * mod - function to find remainder
+ * @a: first integer
+ * @b: second integer
+ * Return: modulus
+ */
+int mod(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a % b);
+	}
+	else
+	{
+		return (0);
+	}
+}

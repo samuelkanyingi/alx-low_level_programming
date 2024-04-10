d7c5214577c56dd633ca604aa63c3b610e2d4bc6
samuelkanyingi <samuelkanyingi2016@gmail.com>
2024-04-09 23:24:51 -0700

    search algos

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,4 +2,5 @@
 #define SEARCH_ALGO
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
 #endif
